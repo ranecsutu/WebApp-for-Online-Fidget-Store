9c4ab9a39aada1f8dfd682c4939e2449d793467e
Razvan <manea.razvan.valentin@gmail.com>
2023-04-01 20:32:46 +0300

    Create Buy Once page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/ranecsutu/fidget/internal/models"
 )
 
 // VirtualTerminal displays the page terminal
@@ -10,7 +12,7 @@ func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) 
 	stringMap["publishable_key"] = app.config.stripe.key
 	if err := app.renderTemplate(w, r, "terminal", &templateData{
 		StringMap: stringMap,
-	}); err != nil {
+	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
@@ -49,7 +51,20 @@ func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request)
 
 // ChargeOnce displays the page to buy one widget
 func (app *application) ChargeOnce(w http.ResponseWriter, r *http.Request) {
-	if err := app.renderTemplate(w, r, "buy-once", nil); err != nil {
+	widget := models.Widget{
+		ID:             1,
+		Name:           "Custom Widget",
+		Description:    "A very nice widget",
+		InventoryLevel: 10,
+		Price:          1000,
+	}
+
+	data := make(map[string]interface{})
+	data["widget"] = widget
+
+	if err := app.renderTemplate(w, r, "buy-once", &templateData{
+		Data: data,
+	}, "stripe-js"); err != nil {
 		app.errorLog.Println(err)
 	}
 }
