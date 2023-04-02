f726e0cfebec1d05cceb25a191ea4af578c8c2d0
Razvan <manea.razvan.valentin@gmail.com>
2023-04-02 17:24:48 +0300

    Add DB connection and write query

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ranecsutu/fidget/internal/driver"
+	"github.com/ranecsutu/fidget/internal/models"
 )
 
 const version = "1.0.0"
@@ -30,6 +31,7 @@ type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
 	version  string
+	DB       models.DBModel
 }
 
 func (app *application) serve() error {
@@ -79,6 +81,7 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 		version:  version,
+		DB:       models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/ranecsutu/fidget/internal/driver"
+	"github.com/ranecsutu/fidget/internal/models"
 )
 
 const version = "1.0.0"
@@ -34,6 +35,7 @@ type application struct {
 	errorLog      *log.Logger
 	templateCache map[string]*template.Template
 	version       string
+	DB            models.DBModel
 }
 
 func (app *application) serve() error {
@@ -87,6 +89,7 @@ func main() {
 		errorLog:      errorLog,
 		templateCache: tc,
 		version:       version,
+		DB:            models.DBModel{DB: conn},
 	}
 
 	err = app.serve()
